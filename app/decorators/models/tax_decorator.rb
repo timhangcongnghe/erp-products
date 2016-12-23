365b84754ccc6677b64b0817dd3dc44b7c99ba48
Luan Pham <luanpm88@gmail.com>
2016-12-23 00:05:52 -0800

    remove redundant has and belongs to relations

--- app/decorators/models/tax_decorator.rb
+++ app/decorators/models/tax_decorator.rb
@@ -1,6 +1,6 @@
 if Erp::Core.available?("taxes")
     Erp::Taxes::Tax.class_eval do
-        has_and_belongs_to_many :products, class_name: 'Erp::Products::Product', :join_table => 'erp_products_customer_taxes'
-        has_and_belongs_to_many :products, class_name: 'Erp::Products::Product', :join_table => 'erp_products_vendor_taxes'
+        has_and_belongs_to_many :customer_products, class_name: 'Erp::Products::Product', :join_table => 'erp_products_customer_taxes'
+        has_and_belongs_to_many :vendor_products, class_name: 'Erp::Products::Product', :join_table => 'erp_products_vendor_taxes'
     end
 end
