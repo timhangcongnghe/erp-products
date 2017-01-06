97177ef66540da4b3cfae096bf1d8a62b52b9943
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-01-06 01:34:39 -0800

    display unit name

--- app/models/erp/products/products_unit.rb
+++ app/models/erp/products/products_unit.rb
@@ -2,5 +2,10 @@ module Erp::Products
   class ProductsUnit < ApplicationRecord
     belongs_to :product
     belongs_to :unit
+    
+    # unit name
+    def unit_name
+			unit.present? ? unit.name : ''
+		end
   end
 end
