4acf76815d084a1436a3b43a78f22a567f1ebd8e
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-01-06 19:16:56 -0800

    add validetes: unit_id, conversion

--- app/models/erp/products/products_unit.rb
+++ app/models/erp/products/products_unit.rb
@@ -3,6 +3,8 @@ module Erp::Products
     belongs_to :product
     belongs_to :unit
     
+    validates :unit_id, :conversion_value, :presence => true
+    
     # unit name
     def unit_name
 			unit.present? ? unit.name : ''
