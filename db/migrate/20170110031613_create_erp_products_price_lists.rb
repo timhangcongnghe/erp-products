5233d0c65b7b058b3bd425934f8044bb33bb0445
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-01-11 18:00:27 -0800

    create migration for price list

---
+++ db/migrate/20170110031613_create_erp_products_price_lists.rb
@@ -0,0 +1,16 @@
+class CreateErpProductsPriceLists < ActiveRecord::Migration[5.0]
+  def change
+    create_table :erp_products_price_lists do |t|
+      t.string :name
+      t.datetime :valid_from
+      t.datetime :valid_to
+      t.boolean :active, default: true
+      t.string :all_warehouses, default: true
+      t.boolean :all_users, default: true
+      t.boolean :all_contact_groups, default: true
+      t.references :creator, index: true, references: :erp_users
+
+      t.timestamps
+    end
+  end
+end
