2d3f269996d1f31eed910555545003a85199d280
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-01-16 23:18:28 -0800

    change column type from string to boolean for all_warehouses

--- db/migrate/20170110031613_create_erp_products_price_lists.rb
+++ db/migrate/20170110031613_create_erp_products_price_lists.rb
@@ -5,7 +5,7 @@ class CreateErpProductsPriceLists < ActiveRecord::Migration[5.0]
       t.datetime :valid_from
       t.datetime :valid_to
       t.boolean :active, default: true
-      t.string :all_warehouses, default: true
+      t.boolean :all_warehouses, default: true
       t.boolean :all_users, default: true
       t.boolean :all_contact_groups, default: true
       t.references :creator, index: true, references: :erp_users
