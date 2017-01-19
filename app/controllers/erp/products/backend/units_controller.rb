c0ddf0bc7cb6bf81d6dcf5d38eccdeda21fa25cf
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-01-19 00:17:31 -0800

    update list action, archive/unarchive for units controller

--- app/controllers/erp/products/backend/units_controller.rb
+++ app/controllers/erp/products/backend/units_controller.rb
@@ -4,8 +4,20 @@ module Erp
   module Products
     module Backend
       class UnitsController < Erp::Backend::BackendController
-        before_action :set_unit, only: [:edit, :update, :destroy]
+        before_action :set_unit, only: [:archive, :unarchive, :edit, :update, :destroy]
+        before_action :set_units, only: [:delete_all, :archive_all, :unarchive_all]
     
+        # GET /units
+        def index
+        end
+        
+        # POST /units/list
+        def list
+          @units = Unit.search(params).paginate(:page => params[:page], :per_page => 3)
+          
+          render layout: nil
+        end
+        
         # GET /units/new
         def new
           @unit = Unit.new
@@ -75,11 +87,85 @@ module Erp
             }
           end
         end
+        
+        # Archive /units/archive?id=1
+        def archive      
+          @unit.archive
+          
+          respond_to do |format|
+          format.json {
+            render json: {
+            'message': t('.success'),
+            'type': 'success'
+            }
+          }
+          end          
+        end
+        
+        # Unarchive /units/unarchive?id=1
+        def unarchive
+          @unit.unarchive
+          
+          respond_to do |format|
+          format.json {
+            render json: {
+            'message': t('.success'),
+            'type': 'success'
+            }
+          }
+          end          
+        end
+        
+        # DELETE /units/delete_all?ids=1,2,3
+        def delete_all         
+          @units.destroy_all
+          
+          respond_to do |format|
+            format.json {
+              render json: {
+                'message': t('.success'),
+                'type': 'success'
+              }
+            }
+          end          
+        end
+        
+        # Archive /units/archive_all?ids=1,2,3
+        def archive_all         
+          @units.archive_all
+          
+          respond_to do |format|
+            format.json {
+              render json: {
+                'message': t('.success'),
+                'type': 'success'
+              }
+            }
+          end          
+        end
+        
+        # Unarchive /units/unarchive_all?ids=1,2,3
+        def unarchive_all
+          @units.unarchive_all
+          
+          respond_to do |format|
+            format.json {
+              render json: {
+                'message': t('.success'),
+                'type': 'success'
+              }
+            }
+          end          
+        end
     
         private
           # Use callbacks to share common setup or constraints between actions.
           def set_unit
             @unit = Unit.find(params[:id])
+          end
+          
+          def set_units
+            @units = Unit.where(id: params[:ids])
           end
     
           # Only allow a trusted parameter "white list" through.
