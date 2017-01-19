e1c8eccc0522f662208462ebac4df59f17b41122
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-01-19 00:23:57 -0800

    add archived column for unit

--- db/migrate/20170105042643_create_erp_products_units.rb
+++ db/migrate/20170105042643_create_erp_products_units.rb
@@ -2,6 +2,7 @@ class CreateErpProductsUnits < ActiveRecord::Migration[5.0]
   def change
     create_table :erp_products_units do |t|
       t.string :name
+      t.boolean :archived, default: false
       t.references :creator, index: true, references: :erp_users
 
       t.timestamps
