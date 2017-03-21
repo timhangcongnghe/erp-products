cb43eb596cdefdeaf6b9b1b79018875a0e5c46cf
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2017-03-21 02:59:40 -0700

    get brand name

--- app/models/erp/products/brand_group_detail.rb
+++ app/models/erp/products/brand_group_detail.rb
@@ -2,5 +2,9 @@ module Erp::Products
   class BrandGroupDetail < ApplicationRecord
     belongs_to :brand_group, class_name: "Erp::Products::BrandGroup"
     belongs_to :brand, class_name: "Erp::Products::Brand"
+    
+    def brand_name
+      brand.name
+    end
   end
 end
