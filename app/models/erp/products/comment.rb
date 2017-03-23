7957fed579358e70931ce7cc63e0866323b0098d
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-03-23 00:20:05 -0700

    update validates format email

--- app/models/erp/products/comment.rb
+++ app/models/erp/products/comment.rb
@@ -4,5 +4,6 @@ module Erp::Products
     belongs_to :parent, class_name: "Erp::Products::Comment", optional: true
     has_many :children, class_name: "Erp::Products::Comment", foreign_key: "parent_id"
     validates :name, :email, :message, :presence => true
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
   end
 end
--- app/models/erp/products/rating.rb
+++ app/models/erp/products/rating.rb
@@ -2,5 +2,6 @@ module Erp::Products
   class Rating < ApplicationRecord
     belongs_to :product, class_name: 'Erp::Products::Product'
     validates :name, :email, :content, :star, :presence => true
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
   end
 end
