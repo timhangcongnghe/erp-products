5e1b2b89900fa629c6dfa850e8a78609cba9a12c
Luan Pham <luanpm88@gmail.com>
2017-04-10 21:02:46 -0700

    update hkerp list layout

--- app/models/erp/products/hkerp_product.rb
+++ app/models/erp/products/hkerp_product.rb
@@ -1,7 +1,10 @@
 module Erp::Products
   class HkerpProduct < ApplicationRecord
+    def self.get_product_by_hkerp_product_id(pid)
+      self.where(hkerp_product_id: pid).first
+    end
     def self.get_status_by_hkerp_product_id(pid)
-      hkerp_product = self.where(hkerp_product_id: pid).first
+      hkerp_product = self.get_product_by_hkerp_product_id(pid)
 
       if hkerp_product.nil?
         '<span class="label label-sm label-danger">new</span>'.html_safe
