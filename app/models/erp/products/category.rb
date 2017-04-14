7494173e3ebe7e4c2f07100cb4c807bf1fd85f57
Luan Pham <luanpm88@gmail.com>
2017-04-13 21:39:27 -0700

    sort properties/groups by created_at

--- app/models/erp/products/category.rb
+++ app/models/erp/products/category.rb
@@ -1,25 +1,25 @@
 module Erp::Products
   class Category < ApplicationRecord
 		include Erp::CustomOrder
-		
+
 		validates :name, :presence => true
-		
+
 		has_many :products
     belongs_to :creator, class_name: "Erp::User"
     belongs_to :parent, class_name: "Erp::Products::Category", optional: true
     has_many :children, class_name: "Erp::Products::Category", foreign_key: "parent_id"
     has_and_belongs_to_many :menus, class_name: "Erp::Menus::Menu"
-    has_and_belongs_to_many :property_groups, class_name: "Erp::Products::PropertyGroup", :join_table => 'erp_products_categories_pgroups'
-    
+    has_and_belongs_to_many :property_groups, -> { order 'erp_products_property_groups.created_at' }, class_name: "Erp::Products::PropertyGroup", :join_table => 'erp_products_categories_pgroups'
+
     after_save :update_level
-    
+
     # get self and children ids
     def get_self_and_children_ids
       ids = [self.id]
       ids += get_children_ids_recursive
       return ids
 		end
-    
+
     # get children ids recursive
     def get_children_ids_recursive
       ids = []
@@ -31,7 +31,7 @@ module Erp::Products
 			end
       return ids
 		end
-    
+
     # update level
     def update_level
 			level = 1
@@ -40,23 +40,23 @@ module Erp::Products
 				level += 1
 				parent = parent.parent
 			end
-			
+
 			level
 		end
-    
+
     # init custom order
     def init_custom_order
 			self.update_column(:custom_order, self.class.maximum("custom_order").to_i + 1)
 		end
-    
+
     # Filters
     def self.filter(query, params)
       params = params.to_unsafe_hash
       and_conds = []
-      
+
       # show archived items condition - default: false
 			show_archived = false
-			
+
 			#filters
 			if params["filters"].present?
 				params["filters"].each do |ft|
@@ -73,7 +73,7 @@ module Erp::Products
 					and_conds << '('+or_conds.join(' OR ')+')' if !or_conds.empty?
 				end
 			end
-      
+
       #keywords
       if params["keywords"].present?
         params["keywords"].each do |kw|
@@ -84,71 +84,71 @@ module Erp::Products
           and_conds << '('+or_conds.join(' OR ')+')'
         end
       end
-      
+
       # join with users table for search creator
       query = query.joins("LEFT JOIN erp_products_categories parents_erp_products_categories ON parents_erp_products_categories.id = erp_products_categories.parent_id")
-      
+
       # showing archived items if show_archived is not true
 			query = query.where(archived: false) if show_archived == false
 
       query = query.where(and_conds.join(' AND ')) if !and_conds.empty?
-      
+
       return query
     end
-    
+
     def self.search(params)
       query = self.all
       query = self.filter(query, params)
-      
+
       # order
       if params[:sort_by].present?
         order = params[:sort_by]
         order += " #{params[:sort_direction]}" if params[:sort_direction].present?
-        
+
         query = query.order(order)
       end
-      
+
       return query
     end
-    
+
     # data for dataselect ajax
     def self.dataselect(keyword='')
       query = self.all
-      
+
       if keyword.present?
         keyword = keyword.strip.downcase
         query = query.where('LOWER(name) LIKE ?', "%#{keyword}%")
       end
-      
+
       query = query.limit(8).map{|category| {value: category.id, text: category.name} }
     end
-    
+
     # product count
     def product_count
 			products.count
 		end
-    
+
     def archive
 			update_columns(archived: true)
 		end
-		
+
 		def unarchive
 			update_columns(archived: false)
 		end
-    
+
     def self.archive_all
 			update_all(archived: true)
 		end
-    
+
     def self.unarchive_all
 			update_all(archived: false)
 		end
-    
+
     # display name
     def parent_name
 			parent.present? ? parent.name : ''
 		end
-		
+
 		# display name with parent
     def full_name
 			names = [self.name]
@@ -159,11 +159,11 @@ module Erp::Products
 			end
 			names.reverse.join(" >> ")
 		end
-    
+
     # Get get all archive
     def self.all_unarchive
 			self.where(archived: false)
 		end
-    
+
   end
 end
