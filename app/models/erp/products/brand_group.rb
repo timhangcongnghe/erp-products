16e184b964174c3e0a703aa466902e30cd3e1f01
Luan Pham <luanpm88@gmail.com>
2017-04-24 20:17:16 -0700

    brand filter

--- app/models/erp/products/brand_group.rb
+++ app/models/erp/products/brand_group.rb
@@ -4,15 +4,16 @@ module Erp::Products
     belongs_to :creator, class_name: "Erp::User"
     has_many :brand_group_details, class_name: "Erp::Products::BrandGroupDetail", dependent: :destroy
     accepts_nested_attributes_for :brand_group_details, :reject_if => lambda { |a| a[:brand_id].blank? }, :allow_destroy => true
-    
+    has_many :brands, through: :brand_group_details
+
     # Filters
     def self.filter(query, params)
       params = params.to_unsafe_hash
       and_conds = []
-      
+
       # show archived items condition - default: false
 			show_archived = false
-			
+
 			#filters
 			if params["filters"].present?
 				params["filters"].each do |ft|
@@ -29,7 +30,7 @@ module Erp::Products
 					and_conds << '('+or_conds.join(' OR ')+')' if !or_conds.empty?
 				end
 			end
-      
+
       #keywords
       if params["keywords"].present?
         params["keywords"].each do |kw|
@@ -40,57 +41,57 @@ module Erp::Products
           and_conds << '('+or_conds.join(' OR ')+')'
         end
       end
-      
+
       # join with users table for search creator
       query = query.joins(:creator)
-      
+
       # showing archived items if show_archived is not true
 			query = query.where(archived: false) if show_archived == false
 
       query = query.where(and_conds.join(' AND ')) if !and_conds.empty?
-      
+
       return query
     end
-    
+
     def self.search(params)
       query = self.all
       query = self.filter(query, params)
-      
+
       # order
       if params[:sort_by].present?
         order = params[:sort_by]
         order += " #{params[:sort_direction]}" if params[:sort_direction].present?
-        
+
         query = query.order(order)
       end
-      
+
       return query
     end
-    
+
     # data for dataselect ajax
     def self.dataselect(keyword='')
       query = self.all
-      
+
       if keyword.present?
         keyword = keyword.strip.downcase
         query = query.where('LOWER(name) LIKE ?', "%#{keyword}%")
       end
-      
+
       query = query.limit(8).map{|brand_group| {value: brand_group.id, text: brand_group.name} }
     end
-    
+
     def archive
 			update_columns(archived: true)
 		end
-		
+
 		def unarchive
 			update_columns(archived: false)
 		end
-    
+
     def self.archive_all
 			update_all(archived: true)
 		end
-    
+
     def self.unarchive_all
 			update_all(archived: false)
 		end
