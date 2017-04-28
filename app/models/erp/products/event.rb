b77ee72f3c2f45323a2cffda303a18d977e3352a
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-04-27 23:29:58 -0700

    update: get events

--- app/models/erp/products/event.rb
+++ app/models/erp/products/event.rb
@@ -9,8 +9,30 @@ module Erp::Products
     has_many :events_products, dependent: :destroy
     accepts_nested_attributes_for :events_products, :reject_if => lambda { |a| a[:product_id].blank? }, :allow_destroy => true
     
-    def self.search(params)
+    def self.events_active
       self.where(archived: false)
+    end
+    
+    def self.search(params)
+      self.events_active
+    end
+    
+    # get future events
+    def self.get_future_events
+      self.events_active.where("from_date >= ?", Time.now)
+                        .order("from_date DESC")
+    end
+    
+    # get current events
+    def self.get_current_events
+      self.events_active.where("from_date <= ? and to_date >= ?", Time.now, Time.now)
+                        .order("from_date DESC")
+    end
+    
+    # get past events
+    def self.get_past_events
+      self.events_active.where("to_date <= ?", Time.now)
+                        .order("from_date DESC")
     end
   end
 end
