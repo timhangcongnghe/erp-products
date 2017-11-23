b15a8c61864e254feb32fccab8c87f113596cd1d
Luan Pham <luanpm88@gmail.com>
2017-11-23 16:39:12 +0700

    fix for demo

--- app/controllers/erp/products/backend/state_checks_controller.rb
+++ app/controllers/erp/products/backend/state_checks_controller.rb
@@ -4,11 +4,11 @@ module Erp
       class StateChecksController < Erp::Backend::BackendController
         before_action :set_state_check, only: [:archive, :unarchive, :status_draft, :status_active, :status_deleted, :edit, :update, :destroy]
         before_action :set_state_checks, only: [:delete_all, :archive_all, :unarchive_all, :status_draft_all, :status_active_all, :status_deleted_all]
-    
+
         # GET /state_checks
         def index
         end
-    
+
         # POST /state_checks/list
         def list
           @state_checks = StateCheck.search(params).paginate(:page => params[:page], :per_page => 10)
@@ -17,28 +17,29 @@ module Erp
           end
           render layout: nil
         end
-        
+
         def state_check_details
           @state_check = StateCheck.find(params[:id])
-          
+
           render layout: nil
         end
-    
+
         # GET /state_checks/new
         def new
           @state_check = StateCheck.new
           @state_check.check_date = Time.now
+          @state_check.employee_id = current_user.id
         end
-    
+
         # GET /state_checks/1/edit
         def edit
         end
-    
+
         # POST /state_checks
         def create
           @state_check = StateCheck.new(state_check_params)
           @state_check.creator = current_user
-    
+
           if @state_check.save
             if request.xhr?
               render json: {
@@ -50,10 +51,10 @@ module Erp
               redirect_to erp_products.edit_backend_state_check_path(@state_check), notice: t('.success')
             end
           else
-            render :new        
+            render :new
           end
         end
-    
+
         # PATCH/PUT /state_checks/1
         def update
           if @state_check.update(state_check_params)
@@ -62,7 +63,7 @@ module Erp
                 status: 'success',
                 text: @state_check.name,
                 value: @state_check.id
-              }              
+              }
             else
               redirect_to erp_products.edit_backend_state_check_path(@state_check), notice: t('.success')
             end
@@ -70,7 +71,7 @@ module Erp
             render :edit
           end
         end
-    
+
         # DELETE /state_checks/1
         def destroy
           @state_check.destroy
@@ -85,7 +86,7 @@ module Erp
             }
           end
         end
-        
+
         # ARCHIVE /state_checks/archive?id=1
         def archive
           @state_check.archive
@@ -98,7 +99,7 @@ module Erp
             }
           end
         end
-        
+
         # UNARCHIVE /state_checks/archive?id=1
         def unarchive
           @state_check.unarchive
@@ -111,7 +112,7 @@ module Erp
             }
           end
         end
-        
+
         # STATUS DRAFT /state_checks/status_draft?id=1
         def status_draft
           @state_check.status_draft
@@ -124,7 +125,7 @@ module Erp
             }
           end
         end
-        
+
         # STATUS ACTIVE /state_checks/status_active?id=1
         def status_active
           @state_check.status_active
@@ -137,7 +138,7 @@ module Erp
             }
           end
         end
-        
+
         # STATUS DELETED /state_checks/status_deleted?id=1
         def status_deleted
           @state_check.status_deleted
@@ -150,11 +151,11 @@ module Erp
             }
           end
         end
-        
+
         # DELETE ALL /state_checks/delete_all?ids=1,2,3
-        def delete_all         
+        def delete_all
           @state_checks.destroy_all
-          
+
           respond_to do |format|
             format.json {
               render json: {
@@ -162,13 +163,13 @@ module Erp
                 'type': 'success'
               }
             }
-          end          
+          end
         end
-        
+
         # ARCHIVE ALL /state_checks/archive_all?ids=1,2,3
-        def archive_all         
+        def archive_all
           @state_checks.archive_all
-          
+
           respond_to do |format|
             format.json {
               render json: {
@@ -176,13 +177,13 @@ module Erp
                 'type': 'success'
               }
             }
-          end          
+          end
         end
-        
+
         # UNARCHIVE ALL /state_checks/unarchive_all?ids=1,2,3
         def unarchive_all
           @state_checks.unarchive_all
-          
+
           respond_to do |format|
             format.json {
               render json: {
@@ -190,13 +191,13 @@ module Erp
                 'type': 'success'
               }
             }
-          end          
+          end
         end
-        
+
         # STATUS DRAFT ALL /state_checks/status_draft_all?ids=1,2,3
-        def status_draft_all         
+        def status_draft_all
           @state_checks.status_draft_all
-          
+
           respond_to do |format|
             format.json {
               render json: {
@@ -204,13 +205,13 @@ module Erp
                 'type': 'success'
               }
             }
-          end          
+          end
         end
-        
+
         # STATUS ACTIVE ALL /state_checks/status_active_all?ids=1,2,3
         def status_active_all
           @state_checks.status_active_all
-          
+
           respond_to do |format|
             format.json {
               render json: {
@@ -218,13 +219,13 @@ module Erp
                 'type': 'success'
               }
             }
-          end          
+          end
         end
-        
+
         # STATUS DELETED ALL /state_checks/status_deleted_all?ids=1,2,3
         def status_deleted_all
           @state_checks.status_deleted_all
-          
+
           respond_to do |format|
             format.json {
               render json: {
@@ -232,9 +233,9 @@ module Erp
                 'type': 'success'
               }
             }
-          end          
+          end
         end
-        
+
         # DATASELECT
         def dataselect
           respond_to do |format|
@@ -243,17 +244,17 @@ module Erp
             }
           end
         end
-    
+
         private
           # Use callbacks to share common setup or constraints between actions.
           def set_state_check
             @state_check = StateCheck.find(params[:id])
           end
-          
+
           def set_state_checks
             @state_checks = StateCheck.where(id: params[:ids])
           end
-    
+
           # Only allow a trusted parameter "white list" through.
           def state_check_params
             params.fetch(:state_check, {}).permit(:name, :check_date, :warehouse_id, :employee_id, :note,
--- app/models/erp/products/state_check.rb
+++ app/models/erp/products/state_check.rb
@@ -6,24 +6,24 @@ module Erp::Products
     STATE_CHECK_STATUS_DRAFT = 'draft'
     STATE_CHECK_STATUS_ACTIVE = 'active'
     STATE_CHECK_STATUS_DELETED = 'deleted'
-    
+
     has_many :state_check_details, inverse_of: :state_check, dependent: :destroy
     accepts_nested_attributes_for :state_check_details, :reject_if => lambda { |a| a[:product_id].blank? || a[:state_id].blank? || a[:quantity].blank? || a[:quantity].to_i <= 0 }, :allow_destroy => true
-    
+
     if Erp::Core.available?("warehouses")
 			validates :warehouse_id, :presence => true
       belongs_to :warehouse, class_name: "Erp::Warehouses::Warehouse"
-      
+
       def warehouse_name
         warehouse.present? ? warehouse.warehouse_name : ''
       end
     end
-    
+
     # Filters
     def self.filter(query, params)
       params = params.to_unsafe_hash
       and_conds = []
-      
+
       #filters
       if params["filters"].present?
         params["filters"].each do |ft|
@@ -39,10 +39,10 @@ module Erp::Products
           and_conds << '('+or_conds.join(' OR ')+')' if !or_conds.empty?
         end
       end
-      
+
       # show archived items condition - default: false
       show_archived = false
-      
+
       #filters
       if params["filters"].present?
         params["filters"].each do |ft|
@@ -59,7 +59,7 @@ module Erp::Products
           and_conds << '('+or_conds.join(' OR ')+')' if !or_conds.empty?
         end
       end
-      
+
       #keywords
       if params["keywords"].present?
         params["keywords"].each do |kw|
@@ -71,94 +71,117 @@ module Erp::Products
         end
       end
 
+      # global filters
+      global_filter = params[:global_filter]
+
+			if global_filter.present?
+
+        # if has period
+        if global_filter[:period].present?
+          period = Erp::Periods::Period.find(global_filter[:period])
+          global_filter[:from_date] = period.from_date
+          global_filter[:to_date] = period.to_date
+        end
+
+				# filter by order from date
+				if global_filter[:from_date].present?
+					query = query.where('check_date >= ?', global_filter[:from_date].to_date.beginning_of_day)
+				end
+
+				# filter by order to date
+				if global_filter[:to_date].present?
+					query = query.where('check_date <= ?', global_filter[:to_date].to_date.end_of_day)
+				end
+			end
+
       # join with users table for search creator
       query = query.joins(:creator)
-      
+
       # join with users table for search employee
       query = query.joins(:employee)
-      
+
       # add conditions to query
       query = query.where(and_conds.join(' AND ')) if !and_conds.empty?
-      
+
       return query
     end
-    
+
     def self.search(params)
       query = self.all
       query = self.filter(query, params)
-      
+
       # order
       if params[:sort_by].present?
         order = params[:sort_by]
         order += " #{params[:sort_direction]}" if params[:sort_direction].present?
-        
+
         query = query.order(order)
       end
-      
+
       return query
     end
-    
+
     # data for dataselect ajax
     def self.dataselect(keyword='')
       query = self.all
-      
+
       if keyword.present?
         keyword = keyword.strip.downcase
         query = query.where('LOWER(name) LIKE ?', "%#{keyword}%")
       end
-      
+
       query = query.limit(8).map{|state_check| {value: state_check.id, text: state_check.name} }
     end
-    
+
     def creator_name
       creator.present? ? creator.name : ''
     end
-    
+
     def employee_name
       employee.present? ? employee.name : ''
     end
-    
+
     # STATUS
     def status_draft
 			update_attributes(status: Erp::Products::StateCheck::STATE_CHECK_STATUS_DRAFT)
 		end
-    
+
     def status_active
 			update_attributes(status: Erp::Products::StateCheck::STATE_CHECK_STATUS_ACTIVE)
 		end
-    
+
     def status_deleted
 			update_attributes(status: Erp::Products::StateCheck::STATE_CHECK_STATUS_DELETED)
 		end
-    
+
     def self.status_draft_all
 			update_all(status: Erp::Products::StateCheck::STATE_CHECK_STATUS_DRAFT)
 		end
-    
+
     def self.status_active_all
 			update_all(status: Erp::Products::StateCheck::STATE_CHECK_STATUS_ACTIVE)
 		end
-    
+
     def self.status_deleted_all
 			update_all(status: Erp::Products::StateCheck::STATE_CHECK_STATUS_DELETED)
 		end
-    
+
     # ARCHIVE
     def archive
 			update_attributes(archived: true)
 		end
-    
+
     def unarchive
 			update_attributes(archived: false)
 		end
-    
+
     def self.archive_all
 			update_all(archived: true)
 		end
-    
+
     def self.unarchive_all
 			update_all(archived: false)
 		end
-    
+
   end
 end
