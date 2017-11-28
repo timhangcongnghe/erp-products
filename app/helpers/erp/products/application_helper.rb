d485553be56c729852f1d1b3dec5e9d295c66476
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-11-28 14:07:52 +0700

    update product link helper

--- app/helpers/erp/products/application_helper.rb
+++ app/helpers/erp/products/application_helper.rb
@@ -1,6 +1,11 @@
 module Erp
   module Products
     module ApplicationHelper
+      # product link helper
+      def product_link(product, text=nil)
+        text = text.nil? ? product.name : text
+        raw "<a href='#{erp_products.backend_product_path(product)}' class='modal-link'>#{text}</a>"
+      end
     end
   end
 end
