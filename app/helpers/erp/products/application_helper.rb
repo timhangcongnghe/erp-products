4458436106d6dd85f8639291fa21d47cb1724612
Son Nguyen <sonnn@hoangkhang.com.vn>
2017-11-30 10:28:11 +0700

    fix conflic product_link

--- app/helpers/erp/products/application_helper.rb
+++ app/helpers/erp/products/application_helper.rb
@@ -2,7 +2,7 @@ module Erp
   module Products
     module ApplicationHelper
       # product link helper
-      def product_link(product, text=nil)
+      def backend_product_link(product, text=nil)
         text = text.nil? ? product.name : text
         raw "<a href='#{erp_products.backend_product_path(product)}' class='modal-link'>#{text}</a>"
       end
