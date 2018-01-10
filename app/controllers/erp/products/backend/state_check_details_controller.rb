a7394b15a8c918c2af957fd4289a78ac8a5a9627
Luan Pham <luanpm88@gmail.com>
2018-01-10 14:46:01 +0700

    default state for state check

--- app/controllers/erp/products/backend/state_check_details_controller.rb
+++ app/controllers/erp/products/backend/state_check_details_controller.rb
@@ -5,7 +5,8 @@ module Erp
         def state_check_detail_line_form
           @state_check_detail = StateCheckDetail.new
           @state_check_detail.product_id = params[:add_value]
-          
+          @state_check_detail.old_state_id = params[:form][:default_state] if params[:form].present?
+
           render partial: params[:partial], locals: {
             state_check_detail: @state_check_detail,
             uid: helpers.unique_id()
