4b86ea40b3fb3bab420ef951895d38639356f942
Nguyen Ngoc Son <kinhdoanh@timhangcongnghe.com>
2020-08-06 02:18:57 +0000

    fix product image uploader

--- app/uploaders/erp/products/product_image_uploader.rb
+++ app/uploaders/erp/products/product_image_uploader.rb
@@ -1,7 +1,6 @@
 module Erp
   module Products
     class ProductImageUploader < CarrierWave::Uploader::Base
-
       # Include RMagick or MiniMagick support:
       # include CarrierWave::RMagick
       include CarrierWave::MiniMagick
@@ -13,7 +12,7 @@ module Erp
         "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
       end
 
-      # For backend list
+      # Start Backend
 			version :small do
 				process :resize_to_fill => [120, 120]
 			end
@@ -21,35 +20,19 @@ module Erp
 			version :medium do
 				process :resize_to_fill => [300, 300]
 			end
+      # End Backend
 			
-			version :thumb75 do
-				process :resize_and_pad => [75, 75, "#FFFFFF", "Center"]
-			end
-			
-			version :thumb99 do
-				process :resize_and_pad => [99, 99, "#FFFFFF", "Center"]
-			end
-			
-			version :thumb170 do
-				process :resize_and_pad => [170, 170, "#FFFFFF", "Center"]
-			end
-			
+			#choose
 			version :thumb193 do
 				process :resize_and_pad => [193, 193, "#FFFFFF", "Center"]
 			end
-			
-			version :thumb268 do
-				process :resize_and_pad => [268, 268, "#FFFFFF", "Center"]
-			end
-
+      
+      #choose
 			version :thumb445 do
 				process :resize_and_pad => [445, 445, "#FFFFFF", "Center"]
 			end
-
-			version :thumb550 do
-				process :resize_and_pad => [550, 550, "#FFFFFF", "Center"]
-			end
 			
+			#choose
 			version :thumb650 do
 				process :resize_and_pad => [650, 650, "#FFFFFF", "Center"] # resize_to_fill => [650, 650]
 			end
