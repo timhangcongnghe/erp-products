a61fb60ddacf08da85e06b7f07aca0bdb15ba4dc
Nguyen Ngoc Son <kinhdoanh@timhangcongnghe.com>
2020-09-08 03:10:02 +0000

    update

--- app/models/erp/products/properties_value.rb
+++ app/models/erp/products/properties_value.rb
@@ -132,5 +132,11 @@ module Erp::Products
         .where(erp_products_products: {is_sold_out: false, category_id: menu.categories.first})
         .where(properties_value: self.id).count
 		end
+    
+    def product_count_by_menu_brand(menu)
+			Erp::Products::ProductsValue.includes(:product)
+        .where(erp_products_products: {is_sold_out: false, category_id: menu.categories.first, brand_id: menu.brand.id})
+        .where(properties_value: self.id).count
+		end
   end
 end
