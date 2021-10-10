76f5359b329f3b8a397c61691b8fefda57a0345e
sonnn0811 <sonnn0811@icloud.com>
2021-10-10 08:32:12 +0700

    update

--- db/migrate/20211002024524_add_custom_title_and_listed_price_to_erp_products_products.rb
+++ db/migrate/20211002024524_add_custom_title_and_listed_price_to_erp_products_products.rb
@@ -1,6 +1,6 @@
 class AddCustomTitleAndListedPriceToErpProductsProducts < ActiveRecord::Migration[5.1]
   def change
     add_column :erp_products_products, :custom_title, :string
-    add_column :erp_products_products, :listed_price, :decimal, default: 1.0
+    add_column :erp_products_products, :listed_price, :decimal, default: 0.0
   end
 end
